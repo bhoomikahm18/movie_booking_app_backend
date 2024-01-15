f65ef2c7a279dae1cf129c043921b84ee3fa0000
Bhoomika <bhoomikahm18@gmail.com>
2024-01-15 14:39:11 +0530

    Delete existing user

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllUsers, singup, updateUser } = require('../controllers/user_controller.js')
+const { getAllUsers, singup, updateUser, deleteUser } = require('../controllers/user_controller.js')
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUsers);
 userRouter.post("/signup", singup);
 userRouter.post("/:id", updateUser);
+userRouter.delete("/:id", deleteUser);
 
 module.exports = userRouter;
