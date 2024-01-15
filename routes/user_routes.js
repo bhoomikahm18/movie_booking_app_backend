a10aa65ea33736ef5c6783e9cf11a93265d4ed1e
Bhoomika <bhoomikahm18@gmail.com>
2024-01-15 14:17:58 +0530

    Updating the user

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAllUsers, singup } = require('../controllers/user_controller.js')
+const { getAllUsers, singup, updateUser } = require('../controllers/user_controller.js')
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUsers);
 userRouter.post("/signup", singup);
+userRouter.post("/:id", updateUser);
 
 module.exports = userRouter;
