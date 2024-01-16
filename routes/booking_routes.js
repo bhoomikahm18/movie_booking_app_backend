56fed50c4a4165a5134cdf161835854c84c29de4
Bhoomika <bhoomikahm18@gmail.com>
2024-01-16 13:21:19 +0530

    Delete bookings

--- routes/booking_routes.js
+++ routes/booking_routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { newBooking } = require('../controllers/booking_controller.js');
+const { newBooking, getBookingById, deleteBooking } = require('../controllers/booking_controller.js');
 
 const bookingsRouter = express.Router();
 
 bookingsRouter.post("/newbooking", newBooking);
+bookingsRouter.get("/:id", getBookingById);
+bookingsRouter.delete("/:id", deleteBooking);
 
 module.exports = bookingsRouter;
